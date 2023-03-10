f7decfede0f29fa6511629a1e2da7acd29d1e467
Gonzalo Morresi <gonzalomorresi@gmail.com>
2023-03-10 11:19:25 -0300

    versión revisada pero con error al leer re.body

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,24 +1,33 @@
 const fs = require('fs');
 
+const datosJson = require("../data.json")
+
 module.exports = {
-  getIndex : (req, res) => {
+  getIndex: (req, res) => {
     console.log("entrando en home");
     res.render('index', { title: 'Predicción CreaTech' });
   },
-  postIndex : (req, res) => {
-    const {year, mileage, city, state, make_number, model_number} = req.body;
+  postIndex: (req, res) => {
+    const { year, mileage, city, state, make_number, model_number } = req.body;
     console.log(req.body);
-    const data = {
-      year,
-      mileage,
-      city,
-      state,
-      make_number,
-      model_number
+    //cambio el estilo usando el modelo que me mostró cristian para guardar la data pero el problema persiste:
+    let mensaje1 = "formulario cargado con éxito"
+    console.log(mensaje1);
+    console.log(year);
+    let data = {
+      year: year,
+      mileage: mileage,
+      city: city,
+      state: state,
+      make_number: make_number,
+      model_number: model_number,
     };
+    datosJson.push(data);
+    let datosFinal = JSON.stringify(datosJson);
+    console.log("estos son los datosFinal json" + datosFinal);
 
     try {
-      fs.writeFileSync('data.json', JSON.stringify(data));
+      fs.writeFileSync('data.json', datosFinal);
       console.log('Data written to file');
       res.redirect('/');
     } catch (error) {
@@ -26,7 +35,8 @@ module.exports = {
       res.status(500).send('Error al guardar los datos');
       return;
     }
-  }}
+  }
+}
 
 
 
